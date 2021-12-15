7024a8d3a09f56f3d046014ef66fd80e288a2c83
ImGabreuw <imgabreuw.dev@gmail.com>
2021-12-15 17:25:59 -0300

    caso de uso 'FetchTopUseCase' (manhwa) implementado

---
+++ comic-domain/src/main/java/br/com/devotaku/comicdomain/usecase/manhwa/FetchTopUseCase.java
@@ -0,0 +1,29 @@
+package br.com.devotaku.comicdomain.usecase.manhwa;
+
+import br.com.devotaku.comicdomain.entity.Manhwa;
+import br.com.devotaku.comicdomain.entity.pagination.PageInfo;
+import br.com.devotaku.comicdomain.gateway.repository.ManhwaRepository;
+import br.com.devotaku.comicdomain.usecase.UseCase;
+
+import java.util.List;
+
+public record FetchTopUseCase(
+        ManhwaRepository manhwaRepository
+) implements UseCase<FetchTopUseCase.InputValues, FetchTopUseCase.OutputValues> {
+
+    @Override
+    public OutputValues execute(InputValues input) {
+        PageInfo pageInfo = input.pageInfo();
+
+        return new OutputValues(
+                manhwaRepository.fetchTopUseCase(pageInfo)
+        );
+    }
+
+    public record InputValues(PageInfo pageInfo) implements UseCase.InputValues {
+    }
+
+    public record OutputValues(List<Manhwa> manhwas) implements UseCase.OutputValues {
+    }
+
+}
---
+++ comic-domain/src/test/java/br/com/devotaku/comicdomain/usecase/manhwa/FetchTopUseCaseTest.java
@@ -0,0 +1,35 @@
+package br.com.devotaku.comicdomain.usecase.manhwa;
+
+import br.com.devotaku.comicdomain.entity.Manhwa;
+import br.com.devotaku.comicdomain.entity.pagination.PageInfo;
+import br.com.devotaku.comicdomain.gateway.ManhwaRepositoryMock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FetchTopUseCaseTest {
+
+    private FetchTopUseCase underTestUseCase;
+
+    @BeforeEach
+    void setUp() {
+        underTestUseCase = new FetchTopUseCase(new ManhwaRepositoryMock());
+    }
+
+    @Test
+    void shouldExecuteFetchTopUseCase() {
+        // given
+        PageInfo pageInfo = PageInfo.create(0, 10);
+        FetchTopUseCase.InputValues input = new FetchTopUseCase.InputValues(pageInfo);
+
+        // when
+        FetchTopUseCase.OutputValues output = underTestUseCase.execute(input);
+
+        // then
+        assertThat(output.manhwas()).hasSize(pageInfo.pageSize());
+        assertThat(output.manhwas()).isSortedAccordingTo(Comparator.reverseOrder());
+    }
+}
