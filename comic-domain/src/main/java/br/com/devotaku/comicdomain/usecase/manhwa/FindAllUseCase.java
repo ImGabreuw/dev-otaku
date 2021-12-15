2ebb165016525df28fa41b8d09083ac107ab3cd7
ImGabreuw <imgabreuw.dev@gmail.com>
2021-12-15 17:01:32 -0300

    Caso de uso 'FindAllUseCase' (manhwa) implementado

---
+++ comic-domain/src/main/java/br/com/devotaku/comicdomain/usecase/manhwa/FindAllUseCase.java
@@ -0,0 +1,29 @@
+package br.com.devotaku.comicdomain.usecase.manhwa;
+
+import br.com.devotaku.comicdomain.entity.Manhwa;
+import br.com.devotaku.comicdomain.entity.pagination.PageInfo;
+import br.com.devotaku.comicdomain.gateway.repository.ManhwaRepository;
+import br.com.devotaku.comicdomain.usecase.UseCase;
+
+import java.util.List;
+
+public record FindAllUseCase(
+        ManhwaRepository manhwaRepository
+) implements UseCase<FindAllUseCase.InputValues, FindAllUseCase.OutputValues> {
+
+    @Override
+    public OutputValues execute(InputValues input) {
+        PageInfo pageInfo = input.pageInfo();
+
+        return new OutputValues(
+                manhwaRepository.findAll(pageInfo)
+        );
+    }
+
+    public record InputValues(PageInfo pageInfo) implements UseCase.InputValues {
+    }
+
+    public record OutputValues(List<Manhwa> manhwas) implements UseCase.OutputValues {
+    }
+
+}
---
+++ comic-domain/src/test/java/br/com/devotaku/comicdomain/usecase/manhwa/FindAllUseCaseTest.java
@@ -0,0 +1,36 @@
+package br.com.devotaku.comicdomain.usecase.manhwa;
+
+import br.com.devotaku.comicdomain.entity.Manhwa;
+import br.com.devotaku.comicdomain.entity.pagination.PageInfo;
+import br.com.devotaku.comicdomain.entity.value.object.Identifier;
+import br.com.devotaku.comicdomain.gateway.ManhwaRepositoryMock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FindAllUseCaseTest {
+
+    private FindAllUseCase underTestUseCase;
+
+    @BeforeEach
+    void setUp() {
+        underTestUseCase = new FindAllUseCase(new ManhwaRepositoryMock());
+    }
+
+    @Test
+    void shouldExecuteFindAllUseCase() {
+        // given
+        PageInfo pageInfo = PageInfo.create(0, 10);
+        FindAllUseCase.InputValues input = new FindAllUseCase.InputValues(pageInfo);
+
+        // when
+        FindAllUseCase.OutputValues output = underTestUseCase.execute(input);
+
+        // then
+        assertThat(output.manhwas()).hasSize(pageInfo.pageSize());
+        assertThat(output.manhwas())
+                .map(Manhwa::getId)
+                .containsExactlyElementsOf(Identifier.generateIDSequenceFrom(pageInfo));
+    }
+}
