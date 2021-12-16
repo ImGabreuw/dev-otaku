02ff7b6c4d4359a6fc13f671ebaac756249d7fee
ImGabreuw <imgabreuw.dev@gmail.com>
2021-12-16 14:50:10 -0300

    caso de uso 'FindAllByFinishedUseCase' (WebToon) implementado

---
+++ comic-domain/src/main/java/br/com/devotaku/comicdomain/usecase/webtoon/FindAllByFinishedUseCase.java
@@ -0,0 +1,29 @@
+package br.com.devotaku.comicdomain.usecase.webtoon;
+
+import br.com.devotaku.comicdomain.entity.WebToon;
+import br.com.devotaku.comicdomain.entity.pagination.PageInfo;
+import br.com.devotaku.comicdomain.gateway.repository.WebToonRepository;
+import br.com.devotaku.comicdomain.usecase.UseCase;
+
+import java.util.List;
+
+public record FindAllByFinishedUseCase(
+        WebToonRepository webToonRepository
+) implements UseCase<FindAllByFinishedUseCase.InputValues, FindAllByFinishedUseCase.OutputValues> {
+
+    @Override
+    public OutputValues execute(InputValues input) {
+        PageInfo pageInfo = input.pageInfo();
+
+        return new OutputValues(
+                webToonRepository.findAllByFinished(pageInfo)
+        );
+    }
+
+    public record InputValues(PageInfo pageInfo) implements UseCase.InputValues {
+    }
+
+    public record OutputValues(List<WebToon> webToons) implements UseCase.OutputValues {
+    }
+
+}
---
+++ comic-domain/src/test/java/br/com/devotaku/comicdomain/usecase/webtoon/FindAllByFinishedUseCaseTest.java
@@ -0,0 +1,34 @@
+package br.com.devotaku.comicdomain.usecase.webtoon;
+
+import br.com.devotaku.comicdomain.entity.WebToon;
+import br.com.devotaku.comicdomain.entity.pagination.PageInfo;
+import br.com.devotaku.comicdomain.gateway.repository.WebToonRepositoryMock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FindAllByFinishedUseCaseTest {
+
+    private FindAllByFinishedUseCase underTestUseCase;
+
+    @BeforeEach
+    void setUp() {
+        underTestUseCase = new FindAllByFinishedUseCase(new WebToonRepositoryMock());
+    }
+
+    @Test
+    void shouldExecuteFindAllByFinishedUseCase() {
+        // given
+        PageInfo pageInfo = PageInfo.create(0, 10);
+        FindAllByFinishedUseCase.InputValues input = new FindAllByFinishedUseCase.InputValues(pageInfo);
+
+        // when
+        FindAllByFinishedUseCase.OutputValues output = underTestUseCase.execute(input);
+
+        // then
+        assertThat(output.webToons()).hasSize(pageInfo.pageSize());
+        assertThat(output.webToons()).allMatch(WebToon::isFinished);
+    }
+
+}
