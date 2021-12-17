a25bf44185c733999c867edc44c7d887b503688b
ImGabreuw <imgabreuw.dev@gmail.com>
2021-12-17 13:43:22 -0300

    Adição do módulo 'anime-domain'

---
+++ anime-domain/src/test/java/br/com/devotaku/animedomain/AnimeDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devotaku.animedomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
