4028f98fe6f79d3b1eb3b62be6d60b3e2a37f880
ImGabreuw <imgabreuw.dev@gmail.com>
2022-02-06 13:34:01 -0300

    Added 'Image' value object and tests created

---
+++ user-domain/src/main/java/br/com/devotaku/userdomain/entities/value/objects/Image.java
@@ -0,0 +1,24 @@
+package br.com.devotaku.userdomain.entities.value.objects;
+
+import br.com.devotaku.shared.validation.SelfValidation;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.NotBlank;
+
+public record Image(
+        @NotBlank(message = "O campo 'ImageUrl' é obrigatório")
+        @URL(
+                message = "O campo 'ImageUrl' deve conter uma URL válida",
+                protocol = "https",
+                host = "cdn.myanimelist.net"
+        )
+        String url
+) implements SelfValidation<Image> {
+
+    public Image(String url) {
+        this.url = url;
+
+        validate(this);
+    }
+
+}
---
+++ user-domain/src/test/java/br/com/devotaku/userdomain/entities/value/objects/ImageTest.java
@@ -0,0 +1,51 @@
+package br.com.devotaku.userdomain.entities.value.objects;
+
+import br.com.devotaku.shared.validation.exception.ValidationException;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ImageTest {
+
+    @Test
+    void shouldCreateImage() {
+        // given
+        var imageUrl = "https://cdn.myanimelist.net/images/manga/1/157897.jpg";
+
+        // when
+        var underTest = new Image(imageUrl);
+
+        // then
+        assertThat(underTest.url()).isEqualTo(imageUrl);
+    }
+
+    @Test
+    void shouldThrowValidationExceptionWhenCreateImageBecauseImageUrlHasInvalidProtocol() {
+        // given
+        var imageUrl = "http://cdn.myanimelist.net/images/manga/1/157897.jpg";
+
+        // when, then
+        assertThatThrownBy(() -> new Image(imageUrl))
+                .isInstanceOf(ValidationException.class)
+                .hasMessage(String.format(
+                        "A classe Image teve suas constraints violadas. [Field[fieldName=url, message=O campo 'ImageUrl' deve conter uma URL válida, value=%s]]",
+                        imageUrl
+                ));
+    }
+
+    @Test
+    void shouldThrowValidationExceptionWhenCreateImageBecauseImageUrlHasInvalidHost() {
+        // given
+        var imageUrl = "https://my-host.com/images/manga/1/157897.jpg";
+
+        // when, then
+        assertThatThrownBy(() -> new Image(imageUrl))
+                .isInstanceOf(ValidationException.class)
+                .hasMessage(String.format(
+                        "A classe Image teve suas constraints violadas. [Field[fieldName=url, message=O campo 'ImageUrl' deve conter uma URL válida, value=%s]]",
+                        imageUrl
+                ));
+    }
+
+}
