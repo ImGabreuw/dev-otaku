b0e63c12689ac276b5e11b01e1e2cc2499ed7c6d
ImGabreuw <imgabreuw.dev@gmail.com>
2022-04-16 22:16:45 -0300

    feat(anime-service): initial configuration

---
+++ anime-service/src/main/java/br/com/devotaku/animeservice/AnimeServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.devotaku.animeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeServiceApplication.class, args);
+	}
+
+}
---
+++ anime-service/src/test/java/br/com/devotaku/animeservice/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devotaku.animeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
