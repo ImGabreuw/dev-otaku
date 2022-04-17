d859951a9b71fdfa81e1933922eb316290bcd9a4
ImGabreuw <imgabreuw.dev@gmail.com>
2022-04-16 22:13:21 -0300

    feat(comic-service): initial configuration

---
+++ comic-service/src/test/java/br/com/devotaku/comicservice/ComicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devotaku.comicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
