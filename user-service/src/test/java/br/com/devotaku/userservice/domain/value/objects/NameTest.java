dea4a6cac4d4937975d0ac2376c0e7c928dc455a
ImGabreuw <imgabreuw.dev@gmail.com>
2022-04-20 19:44:12 -0300

    test(user-service): add new test for Name size check

--- user-service/src/test/java/br/com/devotaku/userservice/domain/value/objects/NameTest.java
+++ user-service/src/test/java/br/com/devotaku/userservice/domain/value/objects/NameTest.java
@@ -1,10 +1,12 @@
 package br.com.devotaku.userservice.domain.value.objects;
 
 import br.com.devotaku.userservice.shared.validation.SelfValidation;
-import br.com.devotaku.userservice.utils.annotation.BlankSource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -21,10 +23,23 @@ class NameTest {
         assertThat(underTest.value()).isEqualTo(name);
     }
 
-    @DisplayName("Should throw ValidationException when create Name")
-    @BlankSource
+    @DisplayName("Should throw ValidationException when create Name because value is null or blank")
+    @NullSource
+    @ValueSource(strings = "   ")
     @ParameterizedTest
-    void shouldThrowValidationExceptionWhenCreateName(String name) {
+    void shouldThrowValidationExceptionWhenCreateNameBecauseIsNullOrBlank(String name) {
+        assertThatThrownBy(() -> new Name(name))
+                .isInstanceOf(SelfValidation.ValidationException.class)
+                .hasMessage(String.format(
+                        "The class Name have its constraints violated. [Field[fieldName=value, message=must not be blank, value=%s]]",
+                        name
+                ));
+    }
+
+    @DisplayName("Should throw ValidationException when create Name because Value is empty")
+    @EmptySource
+    @ParameterizedTest
+    void shouldThrowValidationExceptionWhenCreateNameBecauseValueIsEmpty(String name) {
         assertThatThrownBy(() -> new Name(name))
                 .isInstanceOf(SelfValidation.ValidationException.class);
     }
