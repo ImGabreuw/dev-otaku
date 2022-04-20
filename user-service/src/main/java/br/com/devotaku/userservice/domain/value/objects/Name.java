0bc05c117ee6fd5230149838113067d2dc63b76e
ImGabreuw <imgabreuw.dev@gmail.com>
2022-04-20 19:42:58 -0300

    fix(user-service): add validation to the value size

--- user-service/src/main/java/br/com/devotaku/userservice/domain/value/objects/Name.java
+++ user-service/src/main/java/br/com/devotaku/userservice/domain/value/objects/Name.java
@@ -3,8 +3,13 @@ package br.com.devotaku.userservice.domain.value.objects;
 import br.com.devotaku.userservice.shared.validation.SelfValidation;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
-public record Name(@NotBlank String value) implements SelfValidation<Name> {
+public record Name(
+        @NotBlank
+        @Size(min = 3, max = 50)
+        String value
+) implements SelfValidation<Name> {
 
     public Name(@NotBlank String value) {
         this.value = value;
