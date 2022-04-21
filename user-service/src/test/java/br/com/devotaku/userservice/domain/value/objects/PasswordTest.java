4dfef4b85e2f6de575a42bca0e31c75fd70e98f1
ImGabreuw <imgabreuw.dev@gmail.com>
2022-04-21 14:26:35 -0300

    test(user-service): add tests for 'Password' value object

---
+++ user-service/src/test/java/br/com/devotaku/userservice/domain/value/objects/PasswordTest.java
@@ -0,0 +1,73 @@
+package br.com.devotaku.userservice.domain.value.objects;
+
+import br.com.devotaku.userservice.shared.validation.SelfValidation;
+import br.com.devotaku.userservice.utils.PasswordGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PasswordTest {
+
+    @DisplayName("Should create Password")
+    @Test
+    void shouldCreatePassword() {
+        var password = PasswordGenerator.generateStrongPassword(15);
+
+        Password underTest = new Password(password);
+
+        assertThat(underTest.value()).isEqualTo(password);
+    }
+
+    @DisplayName("Should throw ValidationException when create Password because size is smaller than 8")
+    @Test
+    void shouldThrowValidationExceptionWhenCreatePasswordBecauseSizeIsSmallerThan8() {
+        var password = PasswordGenerator.generateStrongPassword(7);
+
+        assertThatThrownBy(() -> new Password(password))
+                .isInstanceOf(SelfValidation.ValidationException.class)
+                .hasMessageContaining("fieldName=value, message=size must be between 8 and 30, value=%s".formatted(password));
+    }
+
+    @DisplayName("Should throw ValidationException when create Password because does not contain at least one uppercase character")
+    @Test
+    void shouldThrowValidationExceptionWhenCreatePasswordBecauseDoesNotContainAtLeastOneUppercaseCharacter() {
+        var password = PasswordGenerator.generatePasswordWithoutUppercase(8);
+
+        assertThatThrownBy(() -> new Password(password))
+                .isInstanceOf(SelfValidation.ValidationException.class)
+                .hasMessageContaining("fieldName=value, message=must contain at least one uppercase character, value=%s".formatted(password));
+    }
+
+    @DisplayName("Should throw ValidationException when create Password because does not contain at least one lowercase character")
+    @Test
+    void shouldThrowValidationExceptionWhenCreatePasswordBecauseDoesNotContainAtLeastOneLowercaseCharacter() {
+        var password = PasswordGenerator.generatePasswordWithoutLowercase(8);
+
+        assertThatThrownBy(() -> new Password(password))
+                .isInstanceOf(SelfValidation.ValidationException.class)
+                .hasMessageContaining("fieldName=value, message=must contain at least one lowercase character, value=%s".formatted(password));
+    }
+
+    @DisplayName("Should throw ValidationException when create Password because does not contain at least one digit")
+    @Test
+    void shouldThrowValidationExceptionWhenCreatePasswordBecauseDoesNotContainAtLeastOneDigit() {
+        var password = PasswordGenerator.generatePasswordWithoutDigits(8);
+
+        assertThatThrownBy(() -> new Password(password))
+                .isInstanceOf(SelfValidation.ValidationException.class)
+                .hasMessageContaining("fieldName=value, message=must contain at least one digit, value=%s".formatted(password));
+    }
+
+    @DisplayName("Should throw ValidationException when create Password because does not contain at least one symbol")
+    @Test
+    void shouldThrowValidationExceptionWhenCreatePasswordBecauseDoesNotContainAtLeastOneSymbol() {
+        var password = PasswordGenerator.generatePasswordWithoutSymbols(8);
+
+        assertThatThrownBy(() -> new Password(password))
+                .isInstanceOf(SelfValidation.ValidationException.class)
+                .hasMessageContaining("fieldName=value, message=must contain at least one special character, value=%s".formatted(password));
+    }
+
+}
