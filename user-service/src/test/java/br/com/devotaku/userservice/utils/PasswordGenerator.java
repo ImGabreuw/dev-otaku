b060618c9d4a7ef6fd0a7a00ddb56f2b30954a63
ImGabreuw <imgabreuw.dev@gmail.com>
2022-04-21 22:55:00 -0300

    test(user-service): improve password generator

--- user-service/src/test/java/br/com/devotaku/userservice/utils/PasswordGenerator.java
+++ user-service/src/test/java/br/com/devotaku/userservice/utils/PasswordGenerator.java
@@ -1,10 +1,114 @@
 package br.com.devotaku.userservice.utils;
 
 import com.github.javafaker.Faker;
+import org.apache.commons.lang3.CharUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.*;
 
 public class PasswordGenerator {
 
-    private static final Faker FAKER = new Faker();
+    private static final Faker FAKER = Faker.instance();
+
+    public static String generatePassword(int minimumLength, int maximumLength, boolean includeUppercase, boolean includeSymbols, boolean includeDigits) {
+        if (includeSymbols) {
+            var special = new char[]{'!', '@', '#', '$', '%', '^', '&', '*'};
+
+            var characters = FAKER.lorem().characters(minimumLength, maximumLength, true, true);
+
+            while (!characters.matches("(.*[a-z].*)")
+                    || !characters.matches("(.*[A-Z].*)")
+                    || !characters.matches("(.*\\d.*)")
+            ) {
+                characters = FAKER.lorem().characters(minimumLength, maximumLength, true, true);
+            }
+
+            Map<Character, CharInformation> characterAnalysis = new HashMap<>();
+
+            for (int i = 0; i < characters.length(); i++) {
+                var character = characters.charAt(i);
+
+                if (CharUtils.toString(characters.charAt(i)).matches("(.*[a-z].*)")) {
+                    characterAnalysis.put(
+                            character,
+                            new CharInformation(CharType.LETTER_IN_LOWERCASE, i)
+                    );
+                    continue;
+                }
+
+                if (CharUtils.toString(characters.charAt(i)).matches("(.*[A-Z].*)")) {
+                    characterAnalysis.put(
+                            character,
+                            new CharInformation(CharType.LETTER_IN_UPPERCASE, i)
+                    );
+                    continue;
+                }
+
+                if (CharUtils.toString(characters.charAt(i)).matches("(.*\\d.*)")) {
+                    characterAnalysis.put(
+                            character,
+                            new CharInformation(CharType.DIGIT, i)
+                    );
+                }
+            }
+
+            List<CharInformation> lowercase = characterAnalysis.values()
+                    .stream()
+                    .filter(charInformation -> charInformation.type == CharType.LETTER_IN_LOWERCASE)
+                    .toList();
+
+            List<CharInformation> uppercase = characterAnalysis.values()
+                    .stream()
+                    .filter(charInformation -> charInformation.type == CharType.LETTER_IN_UPPERCASE)
+                    .toList();
+
+            List<CharInformation> digits = characterAnalysis.values()
+                    .stream()
+                    .filter(charInformation -> charInformation.type == CharType.DIGIT)
+                    .toList();
+
+            Set<Map.Entry<CharType, Integer>> occurrences = Map.of(
+                    CharType.LETTER_IN_LOWERCASE, lowercase.size(),
+                    CharType.LETTER_IN_UPPERCASE, uppercase.size(),
+                    CharType.DIGIT, digits.size()
+            ).entrySet();
+
+            char[] password = characters.toCharArray();
+
+            occurrences
+                    .stream()
+                    .map(Map.Entry::getValue)
+                    .max(Integer::compareTo)
+                    .ifPresent(moreOccurrence -> {
+                        Map.Entry<CharType, Integer> replaceToSymbol = occurrences.stream().filter(entry -> entry.getValue().equals(moreOccurrence)).findFirst().get();
+
+                        switch (replaceToSymbol.getKey()) {
+                            case LETTER_IN_LOWERCASE -> {
+                                Integer indexReplaceToSymbol = FAKER.random().nextInt(0, lowercase.size() - 1);
+                                CharInformation charInformation = lowercase.get(indexReplaceToSymbol);
+
+                                password[charInformation.index()] = special[FAKER.random().nextInt(0, special.length)];
+                            }
+                            case LETTER_IN_UPPERCASE -> {
+                                Integer indexReplaceToSymbol = FAKER.random().nextInt(0, uppercase.size() - 1);
+                                CharInformation charInformation = uppercase.get(indexReplaceToSymbol);
+
+                                password[charInformation.index()] = special[FAKER.random().nextInt(0, special.length)];
+                            }
+                            case DIGIT -> {
+                                Integer indexReplaceToSymbol = FAKER.random().nextInt(0, digits.size() - 1);
+                                CharInformation charInformation = digits.get(indexReplaceToSymbol);
+
+                                password[charInformation.index()] = special[FAKER.random().nextInt(0, special.length)];
+                            }
+                        }
+                    });
+
+            return StringUtils.join(password, "");
+        }
+
+        return FAKER.lorem().characters(minimumLength, maximumLength, includeUppercase, includeDigits);
+    }
 
     public static String generateStrongPassword(int minimumLength) {
         return generateStrongPassword(minimumLength, ++minimumLength);
@@ -57,6 +161,15 @@ public class PasswordGenerator {
                 .internet()
                 .password(minimumLength, maximumLength, true, false, false)
                 .toUpperCase();
+    }
+
+    private enum CharType {
+        LETTER_IN_LOWERCASE,
+        LETTER_IN_UPPERCASE,
+        DIGIT,
+    }
+
+    private record CharInformation(PasswordGenerator.CharType type, int index) {
     }
 
 }
