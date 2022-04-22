baf18d97e2853e9b6508e8f497cc3c69620117ca
ImGabreuw <imgabreuw.dev@gmail.com>
2022-04-21 22:55:14 -0300

    test(user-service): add tests for 'PasswordGenerator'

---
+++ user-service/src/test/java/br/com/devotaku/userservice/utils/PasswordGeneratorTest.java
@@ -0,0 +1,22 @@
+package br.com.devotaku.userservice.utils;
+
+import br.com.devotaku.userservice.domain.ports.Encryptor;
+import br.com.devotaku.userservice.domain.value.objects.Password;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PasswordGeneratorTest {
+
+    @DisplayName("Show generate a strong password")
+    @Test
+    void showGenerateAStrongPassword() {
+        var password = PasswordGenerator.generatePassword(12, 13, true, true, true);
+
+        Password underTest = new Password(password);
+
+        assertThat(underTest.value()).isEqualTo(Encryptor.DefaultEncryptor.getInstance().encode(password));
+    }
+
+}
