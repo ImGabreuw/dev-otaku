ff793c285393518c5f0ba5ae12a201482869fcee
ImGabreuw <imgabreuw.dev@gmail.com>
2022-04-23 23:21:44 -0300

    feat(anime-service): add utility class to generate 'Title'

---
+++ anime-service/src/test/java/br/com/devotaku/animeservice/utils/TitleUtils.java
@@ -0,0 +1,32 @@
+package br.com.devotaku.animeservice.utils;
+
+import br.com.devotaku.animeservice.domain.entities.value.objects.Title;
+import com.github.javafaker.Faker;
+
+public class TitleUtils {
+
+    private static TitleUtils instance;
+
+    private final Faker faker;
+
+    private TitleUtils() {
+        this.faker = Faker.instance();
+    }
+
+    public static TitleUtils getInstance() {
+        if (instance == null) {
+            instance = new TitleUtils();
+        }
+
+        return instance;
+    }
+
+    public String generateTitleValue() {
+        return faker.name().title();
+    }
+
+    public Title generateTitle() {
+        return new Title(generateTitleValue());
+    }
+
+}
