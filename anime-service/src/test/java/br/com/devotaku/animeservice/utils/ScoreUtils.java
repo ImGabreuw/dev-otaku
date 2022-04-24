9d6548de54373a065c7c9be3189bc27b6b5de50c
ImGabreuw <imgabreuw.dev@gmail.com>
2022-04-23 22:53:50 -0300

    feat(anime-service): add utility class to generate 'Score'

---
+++ anime-service/src/test/java/br/com/devotaku/animeservice/utils/ScoreUtils.java
@@ -0,0 +1,32 @@
+package br.com.devotaku.animeservice.utils;
+
+import br.com.devotaku.animeservice.domain.entities.value.objects.Score;
+import com.github.javafaker.Faker;
+
+public class ScoreUtils {
+
+    private static ScoreUtils instance;
+
+    private final Faker faker;
+
+    private ScoreUtils() {
+        this.faker = Faker.instance();
+    }
+
+    public static ScoreUtils getInstance() {
+        if (instance == null) {
+            instance = new ScoreUtils();
+        }
+
+        return instance;
+    }
+
+    public double generateScoreValue() {
+        return faker.number().randomDouble(2, 0, 10);
+    }
+
+    public Score generateScore() {
+        return new Score(generateScoreValue());
+    }
+
+}
