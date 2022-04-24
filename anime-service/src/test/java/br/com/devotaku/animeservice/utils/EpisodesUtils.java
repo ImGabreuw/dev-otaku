e330dc291aacd010be0e7e01102d5314825ed87a
ImGabreuw <imgabreuw.dev@gmail.com>
2022-04-23 23:32:36 -0300

    feat(anime-service): add utility class to generate 'Episodes'

---
+++ anime-service/src/test/java/br/com/devotaku/animeservice/utils/EpisodesUtils.java
@@ -0,0 +1,42 @@
+package br.com.devotaku.animeservice.utils;
+
+import br.com.devotaku.animeservice.domain.entities.value.objects.Episodes;
+import com.github.javafaker.Faker;
+
+public class EpisodesUtils {
+
+    public static final int EPISODES_DEFAULT_VALUE = 10;
+
+    private static EpisodesUtils instance;
+
+    private final Faker faker;
+
+    private EpisodesUtils() {
+        this.faker = Faker.instance();
+    }
+
+    public static EpisodesUtils getInstance() {
+        if (instance == null) {
+            instance = new EpisodesUtils();
+        }
+
+        return instance;
+    }
+
+    public int generateEpisodesValue(int maximum) {
+        return faker.number().numberBetween(1, maximum);
+    }
+
+    public int generateEpisodesValue() {
+        return faker.number().numberBetween(1, EPISODES_DEFAULT_VALUE);
+    }
+
+    public Episodes generateEpisodes(int maximum) {
+        return new Episodes(generateEpisodesValue(maximum));
+    }
+
+    public Episodes generateEpisodes() {
+        return new Episodes(generateEpisodesValue());
+    }
+
+}
