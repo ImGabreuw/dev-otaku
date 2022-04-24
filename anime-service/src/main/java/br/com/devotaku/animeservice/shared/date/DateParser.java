5fa3796fa009a984cb9f43d4b36e082d7d261e77
ImGabreuw <imgabreuw.dev@gmail.com>
2022-04-24 14:35:52 -0300

    feat(anime-service): add utility class to parse 'LocalDate'

---
+++ anime-service/src/main/java/br/com/devotaku/animeservice/shared/date/DateParser.java
@@ -0,0 +1,17 @@
+package br.com.devotaku.animeservice.shared.date;
+
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class DateParser {
+
+    public static LocalDate parse(String source) {
+        return LocalDate.parse(source, ISO_LOCAL_DATE);
+    }
+
+}
