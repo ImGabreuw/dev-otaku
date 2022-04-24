9db042dc6a2ddc76342a257d218c76f0b051a08f
ImGabreuw <imgabreuw.dev@gmail.com>
2022-04-23 23:22:06 -0300

    test(anime-service): add tests for 'Title' value object

---
+++ anime-service/src/test/java/br/com/devotaku/animeservice/utils/TitleUtilsTest.java
@@ -0,0 +1,34 @@
+package br.com.devotaku.animeservice.utils;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TitleUtilsTest {
+
+    private static TitleUtils underTest;
+
+    @BeforeAll
+    static void beforeAll() {
+        underTest = TitleUtils.getInstance();
+    }
+
+    @DisplayName("Should generate Title value")
+    @Test
+    void shouldGenerateTitleValue() {
+        var value = underTest.generateTitleValue();
+
+        assertThat(value).isNotBlank();
+    }
+
+    @DisplayName("Should generate Title")
+    @Test
+    void shouldGenerateTitle() {
+        var title = underTest.generateTitle();
+
+        assertThat(title.value()).isNotBlank();
+    }
+
+}
