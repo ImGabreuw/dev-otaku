af71bed42f6e022cbe0a15e8404f9f1151a6fe9d
ImGabreuw <imgabreuw.dev@gmail.com>
2022-04-23 22:54:06 -0300

    test(anime-service): add tests for 'ScoreUtils'

---
+++ anime-service/src/test/java/br/com/devotaku/animeservice/utils/ScoreUtilsTest.java
@@ -0,0 +1,34 @@
+package br.com.devotaku.animeservice.utils;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ScoreUtilsTest {
+
+    private static ScoreUtils underTest;
+
+    @BeforeAll
+    static void beforeAll() {
+        underTest = ScoreUtils.getInstance();
+    }
+
+    @DisplayName("Should generate Score value")
+    @Test
+    void shouldGenerateScoreValue() {
+        var value = underTest.generateScoreValue();
+
+        assertThat(value).isBetween(0.00, 10.00);
+    }
+
+    @DisplayName("Should generate Score")
+    @Test
+    void shouldGenerateScore() {
+        var score = underTest.generateScore();
+
+        assertThat(score.value()).isBetween(0.00, 10.00);
+    }
+
+}
