b63ae114113a134309d2dff13056184da9e8fc89
ImGabreuw <imgabreuw.dev@gmail.com>
2022-04-30 22:05:35 -0300

    feat(anime-service): add pagination info

---
+++ anime-service/src/main/java/br/com/devotaku/animeservice/shared/page/PageInfo.java
@@ -0,0 +1,44 @@
+package br.com.devotaku.animeservice.shared.page;
+
+import br.com.devotaku.animeservice.shared.validation.SelfValidation;
+
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+
+public record PageInfo(
+        @PositiveOrZero
+        int pageNumber,
+
+        @Positive
+        int pageSize,
+
+        int pageInterval,
+
+        int firstElementPosition,
+
+        int lastElementPosition
+) implements SelfValidation<PageInfo> {
+
+    public PageInfo(int pageNumber, int pageSize, int pageInterval, int firstElementPosition, int lastElementPosition) {
+        this.pageNumber = pageNumber;
+        this.pageSize = pageSize;
+        this.pageInterval = pageInterval;
+        this.firstElementPosition = firstElementPosition;
+        this.lastElementPosition = lastElementPosition;
+
+        validate(this);
+    }
+
+    public static PageInfo create(int pageNumber, int pageSize) {
+        int pageInterval = pageSize - 1;
+        int lastElementPosition = (pageNumber + 1) * pageInterval + (pageNumber + 1);
+        int firstElementPosition = lastElementPosition - pageInterval;
+
+        return new PageInfo(pageNumber, pageSize, pageInterval, lastElementPosition, firstElementPosition);
+    }
+
+    public static PageInfo createDefault() {
+        return create(0, 20);
+    }
+
+}
