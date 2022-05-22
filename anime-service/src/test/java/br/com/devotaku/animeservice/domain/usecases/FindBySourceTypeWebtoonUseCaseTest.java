f219993a61fdaca68f1c2d284a43c68a66b1c435
ImGabreuw <imgabreuw.dev@gmail.com>
2022-05-22 17:22:07 -0300

    test(anime-service): add tests for 'FindBySourceTypeWebtoonUseCase'

---
+++ anime-service/src/test/java/br/com/devotaku/animeservice/domain/usecases/FindBySourceTypeWebtoonUseCaseTest.java
@@ -0,0 +1,45 @@
+package br.com.devotaku.animeservice.domain.usecases;
+
+import br.com.devotaku.animeservice.domain.entities.Anime;
+import br.com.devotaku.animeservice.domain.ports.repositories.AnimeRepository;
+import br.com.devotaku.animeservice.shared.page.PageInfo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static br.com.devotaku.animeservice.domain.entities.enums.SourceType.WEBTOON;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class FindBySourceTypeWebtoonUseCaseTest {
+
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    private FindBySourceTypeWebtoonUseCase underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new FindBySourceTypeWebtoonUseCase(animeRepository);
+    }
+
+    @DisplayName("Should execute FindBySourceTypeWebtoonUseCase")
+    @Test
+    void shouldExecuteFindBySourceTypeWebtoonUseCase() {
+        var page = PageInfo.createDefault();
+
+        var input = new FindBySourceTypeWebtoonUseCase.InputValues(page);
+
+        var output = underTest.execute(input);
+
+        assertThat(output.animeList())
+                .map(Anime::getSource)
+                .containsOnly(WEBTOON);
+    }
+
+}
