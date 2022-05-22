25145dc5899702ecc609527e63386afeb9b04023
ImGabreuw <imgabreuw.dev@gmail.com>
2022-05-22 17:21:48 -0300

    fix(anime-service): change 'SourceType' random generation algorithm

--- anime-service/src/main/java/br/com/devotaku/animeservice/shared/generators/SourceTypeUtils.java
+++ anime-service/src/main/java/br/com/devotaku/animeservice/shared/generators/SourceTypeUtils.java
@@ -1,17 +1,15 @@
 package br.com.devotaku.animeservice.shared.generators;
 
 import br.com.devotaku.animeservice.domain.entities.enums.SourceType;
-import com.github.javafaker.Faker;
+import br.com.devotaku.animeservice.shared.helper.RandomHelper;
+import lombok.NoArgsConstructor;
 
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
 public class SourceTypeUtils {
 
     private static SourceTypeUtils instance;
-
-    private final Faker faker;
-
-    private SourceTypeUtils() {
-        this.faker = Faker.instance();
-    }
 
     public static SourceTypeUtils getInstance() {
         if (instance == null) {
@@ -24,7 +22,7 @@ public class SourceTypeUtils {
     public SourceType generateSourceType() {
         var sourceTypes = SourceType.values();
 
-        return sourceTypes[faker.number().numberBetween(0, sourceTypes.length - 1)];
+        return sourceTypes[RandomHelper.number(0, sourceTypes.length - 1)];
     }
 
 }
